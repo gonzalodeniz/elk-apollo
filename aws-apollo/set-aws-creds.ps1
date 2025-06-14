06328fda0154b3b36c050bd510dff737785966c9
Gonzalo Déniz <soygonzalodeniz@gmail.com>
2025-06-14 20:39:31 +0100

    Se eliminan las credenciales de AWS

--- aws-apollo/set-aws-creds.ps1
+++ aws-apollo/set-aws-creds.ps1
@@ -1,10 +1,10 @@
 # set-aws-creds.ps1
 
 # Reemplaza los valores con tus claves reales
-$env:AWS_ACCESS_KEY_ID = "xxxxxxx"
-$env:AWS_SECRET_ACCESS_KEY = "xxxxxxx"
-$env:AWS_SESSION_TOKEN  = "xxxxxxx"
-$env:AWS_REGION = "us-east-1"
+# $env:AWS_ACCESS_KEY_ID = "xxxxxxx"
+# $env:AWS_SECRET_ACCESS_KEY = "xxxxxxx"
+# $env:AWS_SESSION_TOKEN  = "xxxxxxx"
+# $env:AWS_REGION = "us-east-1"
 
 
 Write-Host "Variables AWS configuradas para esta sesion."
