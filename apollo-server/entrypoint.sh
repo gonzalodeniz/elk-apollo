5fc1b1b6a4f223499b54a1a4396c3f80625c873d
Gonzalo Déniz <soygonzalodeniz@gmail.com>
2025-06-14 17:19:36 +0100

    Añade metricbeat

--- apollo-server/entrypoint.sh
+++ apollo-server/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 # Inicia filebeat en segundo plano
 /usr/bin/filebeat -e -c /etc/filebeat/filebeat.yml &
+/usr/bin/metricbeat -e -c /etc/metricbeat/metricbeat.yml &
 
 # Ejecuta la aplicación Node.js
-exec npm start
+exec npm start >> /var/log/apollo-server.log 2>&1
